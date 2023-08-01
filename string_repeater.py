5e6dd8d29006cc7a59d4e2a6368a5e959aae42d7
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-08-01 16:04:39 +0800

    string_repeater

---
+++ string_repeater.py
@@ -0,0 +1,12 @@
+
+
+# string repeater 
+# this function take two parameters text and number and then based on the number will repeat the string
+
+def repeat_string(text, number) :  # passing text and number we need to repeat the text 
+
+    return text * number  # multiply the text with the number 
+
+string_multiply_result = repeat_string('Hello\t', 4)  # call the function and pass the arguments values.
+print(string_multiply_result)
+
