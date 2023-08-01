723e1c2a5dddf9a9c2562acbafa62d2c0fc6e844
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-08-01 15:51:18 +0800

    inverted_pright_half_pyramid_pattern

---
+++ inverted_pright_half_pyramid_pattern.py
@@ -0,0 +1,15 @@
+ 
+
+n = 10
+
+def invertedPatternRightHalfPyramid(n) :
+    
+    for i in range(n) :
+         
+        for j in range(n - i) :
+            print('*', end='' )
+        print()
+
+invertedPatternRightHalfPyramid(5)
+
+ 
