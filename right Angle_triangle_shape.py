817acea5433836ac4738b9318f5c80a6891351d4
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-08-01 16:46:53 +0800

    right Angle_triangle_shape

--- right Angle_triangle_shape.py
+++ right Angle_triangle_shape.py
@@ -1,7 +1,15 @@
 
-n = 3 
+def getNumberOfRows(): 
+    
+    try :
+        row = int(input('enter the number of row ? ').strip())
 
-for i in range(0, n+1) :
-    for j in range((1 * i) + i-1) :
+        return row
+    except :
+        
+        return 'enter a digits only'
+
+for row in range(0, getNumberOfRows()+1) :
+    for col in range((1 * row) + row-1) :
         print('*', end=' ')
     print()
