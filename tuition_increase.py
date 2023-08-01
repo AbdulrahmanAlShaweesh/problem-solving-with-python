f270f97c60a64585fccd6e7dc88885811d77dca8
Alshwish Abdulrahman <alshwishabdulrahman@Alshwishs-MacBook-Pro.local>
2023-08-01 16:02:06 +0800

    tuition_increase

---
+++ tuition_increase.py
@@ -0,0 +1,15 @@
+
+
+# Tuition Increase
+
+
+course_fee = 16000
+
+for year in range(1,6) :
+    
+    # calculate the yearly increaement 3% 
+    increasement = course_fee * 0.03 
+    
+    # total fee 
+    course_fee += increasement
+    print(course_fee)
